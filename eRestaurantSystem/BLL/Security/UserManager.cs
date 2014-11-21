51f4c5763ca527bc1aa09639c247116cfd060c13
Poting Chiang <poting.chiang@gmail.com>
2014-11-21 08:31:46 -0700

    20141121_LatestVersion

---
+++ eRestaurantSystem/BLL/Security/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using eRestaurantSystem.DAL;
+using eRestaurantSystem.DAL.Security;
+using eRestaurantSystem.Entities.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+#endregion
+
+namespace eRestaurantSystem.BLL.Security
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+    }
+}
---
+++ eRestaurantSystem/Entities/Security/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region
+using Microsoft.AspNet.Identity.EntityFramework;
+#endregion
+
+namespace eRestaurantSystem.Entities.Security
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
